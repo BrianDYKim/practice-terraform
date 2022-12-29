41ff377e3ae8e762a7d0f87ef334a9b3d8d75626
BrianDYKim <18k7102dy@naver.com>
2022-12-29 21:38:08 +0900

    06-variable 파일 생성

---
+++ 06-variable/main.tf
@@ -0,0 +1,99 @@
+provider "aws" {
+  region = "ap-northeast-2"
+}
+
+# vpc module
+module "vpc" {
+  source  = "tedilabs/network/aws//modules/vpc"
+  version = "0.24.0"
+
+  name       = "terraform-example"
+  cidr_block = "20.0.0.0/16"
+
+  internet_gateway_enabled = true
+
+  dns_hostnames_enabled = true
+  dns_support_enabled   = true
+
+  tags = {}
+}
+
+# public subnet group
+module "subnet_group__public" {
+  source  = "tedilabs/network/aws//modules/subnet-group"
+  version = "0.24.0"
+
+  name                    = "${module.vpc.name}-public"
+  vpc_id                  = module.vpc.id
+  map_public_ip_on_launch = true
+
+  subnets = {
+    "${module.vpc.name}-public-001/az1" = {
+      cidr_block           = "20.0.0.0/24"
+      availability_zone_id = "apne2-az1"
+    }
+
+    "${module.vpc.name}-public-002/az3" = {
+      cidr_block          = "20.0.1.0/24"
+      avalibility_zone_id = "apne2-az3"
+    }
+  }
+
+  tags = {}
+}
+
+module "subnet_group__private" {
+  source  = "tedilabs/network/aws//modules/subnet-group"
+  version = "0.24.0"
+
+  name                    = "${module.vpc.name}-private"
+  vpc_id                  = module.vpc.id
+  map_public_ip_on_launch = false
+
+  subnets = {
+    "${module.vpc.name}-private-001/az1" = {
+      cidr_block          = "20.0.10.0/24"
+      avalibility_zone_id = "apne2-az1"
+    }
+
+    "${module.vpc.name}-private-002/az3" = {
+      cidr_block          = "20.0.11.0/24"
+      avalibility_zone_id = "apne2-az3"
+    }
+  }
+
+  tags = {}
+}
+
+module "route_table__public" {
+  source  = "tedilabs/network/aws//modules/route-table"
+  version = "0.24.0"
+
+  name   = "${module.vpc.name}-public"
+  vpc_id = module.vpc.id
+
+  subnets = module.subnet_group__public.ids
+
+  ipv4_routes = [
+    {
+      cidr_block = "0.0.0.0/0"
+      gateway_id = module.vpc.internet_gateway_id
+    }
+  ]
+
+  tags = {}
+}
+
+module "route_table__private" {
+  source  = "tedilabs/network/aws//modules/route-table"
+  version = "0.24.0"
+
+  name   = "${module.vpc.name}-private"
+  vpc_id = module.vpc.id
+
+  subnets = module.subnet_group__private.ids
+
+  ipv4_routes = []
+
+  tags = {}
+}
