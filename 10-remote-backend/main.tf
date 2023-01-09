85e53cceb869919abe70c73f7319cccbc9659ec2
BrianDYKim <18k7102dy@naver.com>
2023-01-10 03:54:12 +0900

    terraform remote backend 연동 테스트 완료

--- 10-remote-backend/main.tf
+++ 10-remote-backend/main.tf
@@ -1,4 +1,3 @@
-// Remote Backend를 S3로 관리
 terraform {
   backend "remote" {
     hostname     = "app.terraform.io"
@@ -48,4 +47,38 @@ resource "aws_iam_user" "this" {
     level = each.value.level
     role  = each.value.role
   }
+}
+
+resource "aws_iam_user_group_membership" "this" {
+  for_each = {
+    for user in var.users :
+    user.name => user
+  }
+
+  user   = each.key
+  groups = each.value.is_developer ? [aws_iam_group.develop.name, aws_iam_group.employee.name] : [aws_iam_group.employee.name]
+}
+
+// developer인 user들만 필터링해서 locals에 저장
+locals {
+  developers = [
+    for user in var.users :
+    user
+    if user.is_developer
+  ]
+}
+
+// developer들에게 권한 부여
+resource "aws_iam_user_policy_attachment" "developer" {
+  for_each = {
+    for user in local.developers :
+    user.name => user
+  }
+
+  user       = each.key
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+
+  depends_on = [
+    aws_iam_user.this
+  ]
 }
---
+++ 10-remote-backend/terraform.tfvars
@@ -0,0 +1,26 @@
+users = [
+  {
+    name         = "alice"
+    level        = 1
+    role         = "Intern"
+    is_developer = true
+  },
+  {
+    name         = "tony"
+    level        = 4
+    role         = "Devops"
+    is_developer = true
+  },
+  {
+    name         = "cindy"
+    level        = 9
+    role         = "Administrator"
+    is_developer = false
+  },
+  {
+    name         = "hoon"
+    level        = 3
+    role         = "Marketing"
+    is_developer = true
+  },
+]
